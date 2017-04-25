89c10c5981347d392eb6406b7339b8d636492332
tanujv <tanujv@tanujv.vakrangee.com>
2017-04-25 17:52:39 +0530

    icon changes

---
+++ eclab1/src/main/java/in/vakrangee/vkms/eclab1/Graph.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.vakrangee.vkms.eclab1;
+
+import java.util.List;
+
+/**
+ *
+ * @author tanujv
+ */
+
+public class Graph {
+
+    private final List<Vertex> vertexes;
+    private final List<Edge> edges;
+
+    public Graph(List<Vertex> vertexes, List<Edge> edges) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+    }
+
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+}
